536e7d59200f78508d17f5ee29b10c28b1a0f551
ivandax <ivandax_89@hotmail.com>
2023-12-03 09:28:33 +0100

    Add registration data

--- README.md
+++ README.md
@@ -1,24 +1,13 @@
-# tree-nation
+# tree nation
 
-## Project setup
 ```
 yarn install
-```
-
-### Compiles and hot-reloads for development
-```
 yarn serve
 ```
 
-### Compiles and minifies for production
-```
-yarn build
-```
+Some comments:
 
-### Lints and fixes files
-```
-yarn lint
-```
+ - My experience is mainly with React. The building of this demo relied heavily on the assistance of chat GPT. 
 
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+It should look like this:
+
--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,12 +1,22 @@
 <template>
   <div class="user-info">
-    <div class="details">
+    <div class="graph-details">
+      <div>Registrations per Hour</div>
+      <ul>
+        <li v-for="(registration, index) in registrationsPerHour" :key="index">
+          {{ Object.keys(registration)[0] }}:
+          {{ Object.values(registration)[0] }}
+        </li>
+      </ul>
+    </div>
+
+    <div class="last-user-details">
       <div class="user-name-and-avatar">
-        <p><strong>Last registered user:</strong> {{ userData.name }}</p>
+        <div><strong>Last registered user:</strong> {{ userData.name }}</div>
         <img :src="userData.profile_picture" alt="Profile Picture" />
       </div>
 
-      <p><strong>Registered At:</strong> {{ userData.registered_at }}</p>
+      <div><strong>Registered At:</strong> {{ userData.registered_at }}</div>
     </div>
   </div>
 </template>
@@ -24,10 +34,13 @@ export default {
       registered_at: "",
     });
 
+    const registrationsPerHour = ref([]);
+
     const fetchData = () => {
       import("@/data/data.json")
         .then((data) => {
           userData.value = data.default.lastRegisteredUser;
+          registrationsPerHour.value = data.default.registrations_per_hour;
         })
         .catch((error) => {
           console.error("Error fetching data:", error);
@@ -40,25 +53,36 @@ export default {
 
     return {
       userData,
+      registrationsPerHour,
     };
   },
 };
 </script>
 
 <style  lang="scss">
+@mixin box-mixin {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 20px;
+  border-radius: 8px;
+  border: 2px solid black;
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
+  background-color: white;
+  margin: 12px;
+}
+
 .user-info {
   display: flex;
+  flex-direction: column;
 
-  .details {
-    display: flex;
-    flex-direction: column;
-    align-items: flex-start;
-    padding: 20px;
-    border-radius: 8px;
-    border: 2px solid black;
-    box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2);
-    background-color: white;
-    margin: 12px;
+  .graph-details {
+    @include box-mixin; // Include the mixin here
+  }
+
+  .last-user-details {
+    @include box-mixin; // Include the mixin here
+    width: 50%;
 
     .user-name-and-avatar {
       display: flex;
